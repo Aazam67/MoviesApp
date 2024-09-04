1ab39b2007222e7c71b76c328433cf02245a20df
Aazam AB <azam8788@gmail.com>
2024-09-04 17:16:54 +0200

    setting new app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-    </>
-  );
+  return <Button colorScheme="blue">Button</Button>;
 }
 
 export default App;
